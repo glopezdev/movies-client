83891b2f890c62b66b482a235cd5a101c2597556
glopez <glopez@devspark.com>
2016-02-23 09:03:45 -0300

    wip uirouter not being called

--- app/app.js
+++ app/app.js
@@ -1,53 +1,22 @@
-var app = angular.module('movieClientApp', ['ngTouch', 'ui.grid']);
-
-app.controller('MainCtrl', ['$scope', function ($scope) {
-
-  $scope.someProp = 'Click!';
-  $scope.showMe = function(){
-    alert($scope.someProp);
-  };
-
-  $scope.gridOptions = {};
-
-  //you can override the default assignment if you wish
-  //$scope.gridOptions.appScopeProvider = someOtherReference;
-
-  $scope.gridOptions.columnDefs = [
-    { name: 'name' },
-    { name: 'gender'},
-    { name: 'year'},
-    { name: 'btn1',cellTemplate:'<button class="btn primary" ng-click="grid.appScope.showMe()">Click Me</button>' },
-    { name: 'btn2',cellTemplate:'<button class="btn primary" ng-click="grid.appScope.showMe()">Click Me</button>' },
-    { name: 'btn3',cellTemplate:'<button class="btn primary" ng-click="grid.appScope.showMe()">Click Me</button>' }
-  ];
-
-   $scope.gridOptions.data = [
-     {
-       name: "Pulp Fiction",
-       year: "1994",
-       gender: "action"
-     },
-     {
-       name: "The Godfather",
-       year: "1972",
-       gender: "action"
-     },
-     {
-       name: "Fight Club",
-       year: "1999",
-       gender: "action"
-     },
-     {
-       name: "Star Wars: Episode V - The Empire Strikes Back",
-       year: "1980",
-       gender: "Sci-Fi"
-     }
-   ];
-
-/*
-  $http.get('/data/100.json')
-    .success(function(data) {
-      $scope.gridOptions.data = data;
-    });
-*/
+var app = angular.module('movieClientApp', ['ui.router']);
+app.config(["$stateProvider","$urlRouterProvider",
+  function($stateProvider, $urlRouterProvider) {
+    console.log("state provider!!!");
+    alert("state provider!!!");
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/list");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('list', {
+      url: "/list",
+      templateUrl: "list/list.html",
+  //    controller: "ListController"
+    })
+    .state('state2', {
+      url: "/state2",
+      templateUrl: "state2.html"
+    })
 }]);
+console.log("appp.js");
