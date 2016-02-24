94b0722a9b84a1f0dff25d7d1c33b261ec2f249a
glopez <glopez@devspark.com>
2016-02-24 10:20:19 -0300

    refactor

--- app/app.js
+++ app/app.js
@@ -2,75 +2,86 @@
   var app = angular.module('movieClientApp', ['ngTouch', 'ui.grid','movieClientService']);
   app.controller('MainCtrl', ['$scope','Movies', function ($scope, Movies) {
 
-    $scope.showMe = function () {
-      alert('Click!');
-    };
-
+    $scope.showMovie = showMovie;
+    $scope.editMovie = editMovie;
+    $scope.deleteMovie = deleteMovie;
     $scope.gridOptions = {};
 
-    $scope.gridOptions.onRegisterApi = function (gridApi) {
-      $scope.gridApi = gridApi;
-      $scope.gridApi.grid.registerRowsProcessor($scope.singleFilter, 200);
+    return init();
+
+    function init () {
+      $scope.gridOptions.onRegisterApi = function (gridApi) {
+        $scope.gridApi = gridApi;
+        $scope.gridApi.grid.registerRowsProcessor($scope.singleFilter, 200);
+      }
+
+      $scope.gridOptions.columnDefs = [
+        {name: 'name'},
+        {name: 'gender'},
+        {name: 'year'},
+        {name: 'details', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.showMovie()">View Me</button>'},
+        {name: 'edit', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.editMovie()">Edit Me</button>'},
+        {name: 'delete', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.deleteMovie(row)">Delete Me</button>'}
+      ];
+
+      $scope.gridOptions.data = [
+        {
+          name: "Pulp Fiction",
+          year: "1994",
+          gender: "action"
+        },
+        {
+          name: "The Godfather",
+          year: "1972",
+          gender: "action"
+        },
+        {
+          name: "Fight Club",
+          year: "1999",
+          gender: "action"
+        },
+        {
+          name: "Star Wars: Episode V - The Empire Strikes Back",
+          year: "1980",
+          gender: "Sci-Fi"
+        }
+      ];
+
+      Movies.query(function(results){
+        console.log("Query",arguments);
+        results.forEach(function(result){
+          $scope.gridOptions.data.push(result);
+        })
+      });
+
+      $scope.$watch('filterValue', function () {
+        $scope.gridApi.grid.refresh();
+      });
+
+      $scope.singleFilter = function (renderableRows) {
+        var matcher = new RegExp($scope.filterValue);
+        renderableRows.forEach(function (row) {
+          if (!row.entity["name"].match(matcher)) {
+            row.visible = false;
+          }
+        });
+        return renderableRows;
+      };
     }
 
-    $scope.gridOptions.columnDefs = [
-      {name: 'name'},
-      {name: 'gender'},
-      {name: 'year'},
-      {name: 'btn1', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.showMe()">Click Me</button>'},
-      {name: 'btn2', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.showMe()">Click Me</button>'},
-      {name: 'btn3', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.showMe()">Click Me</button>'}
-    ];
+    function deleteMovie (row) {
+      console.log("delete Movie ",row);
+      // Movies.delete(row.entity._id);
+      row.entity.$delete();
+    }
 
-    $scope.gridOptions.data = [
-      {
-        name: "Pulp Fiction",
-        year: "1994",
-        gender: "action"
-      },
-      {
-        name: "The Godfather",
-        year: "1972",
-        gender: "action"
-      },
-      {
-        name: "Fight Club",
-        year: "1999",
-        gender: "action"
-      },
-      {
-        name: "Star Wars: Episode V - The Empire Strikes Back",
-        year: "1980",
-        gender: "Sci-Fi"
-      }
-    ];
+    function showMovie() {
+      alert('showMovie');
+    }
 
-    Movies.query(function(results){
-      console.log("Query",arguments);
-      results.forEach(function(result){
-        $scope.gridOptions.data.push(result);
-      })
-    });
+    function editMovie() {
+      alert('editMovie!');
+    }
 
-    $scope.$watch('filterValue', function () {
-      $scope.gridApi.grid.refresh();
-    });
-
-    $scope.singleFilter = function (renderableRows) {
-      var matcher = new RegExp($scope.filterValue);
-      renderableRows.forEach(function (row) {
-        if (!row.entity["name"].match(matcher)) {
-          row.visible = false;
-        }
-      });
-      return renderableRows;
-    };
-
-    /*
-     $http.get('/data/100.json')
-     .success(function(data) {
-     $scope.gridOptions.data = data;
-     });
-     */
   }]);
 })();
--- app/movie.service.js
+++ app/movie.service.js
@@ -1,10 +1,17 @@
 (function(){
   var app = angular.module('movieClientService', ['ngResource']);
   app.factory('Movies', ['$resource', function($resource) {
-    return $resource('http://localhost:3000/api/movies/:id',{id:'@_id'},{
+    return $resource('http://localhost:3000/api/movies/:id/',
+      {id:'@_id'},
+      {
         update : {
           method: 'PUT'
+        },
+        delete : {
+          method: 'DELETE'
         }
     })
-  }])
+  }]).config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+  }]);
 })();
