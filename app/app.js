a40870322b4f374bcee3944ca740a4d9d3e7f909
glopez <glopez@devspark.com>
2016-02-24 10:46:40 -0300

    fixing table columns

--- app/app.js
+++ app/app.js
@@ -2,86 +2,65 @@
   var app = angular.module('movieClientApp', ['ngTouch', 'ui.grid','movieClientService']);
   app.controller('MainCtrl', ['$scope','Movies', function ($scope, Movies) {
 
-    $scope.showMovie = showMovie;
-    $scope.editMovie = editMovie;
-    $scope.deleteMovie = deleteMovie;
-    $scope.gridOptions = {};
+  $scope.showMovie = showMovie;
+  $scope.editMovie = editMovie;
+  $scope.deleteMovie = deleteMovie;
+  $scope.gridOptions = {};
+  $scope.gridOptions.data = [];
+  $scope.gridOptions.onRegisterApi = onRegisterApi;
+  $scope.gridOptions.columnDefs = [
+    {name: 'id'},
+    {name: 'name'},
+    {name: 'details', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.showMovie()">View Me</button>'},
+    {name: 'edit', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.editMovie()">Edit Me</button>'},
+    {name: 'delete', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.deleteMovie(row)">Delete Me</button>'}
+  ];
+  $scope.singleFilter = filter;
 
-    return init();
+  return init();
 
-    function init () {
-      $scope.gridOptions.onRegisterApi = function (gridApi) {
-        $scope.gridApi = gridApi;
-        $scope.gridApi.grid.registerRowsProcessor($scope.singleFilter, 200);
+  function init () {
+    Movies.query(function(results){
+      console.log("Query",arguments);
+      results.forEach(function(result){
+        result.id = result._id; //ugly hack, should be dealt with on the grid
+        $scope.gridOptions.data.push(result);
+      })
+    });
+
+    $scope.$watch('filterValue', function () {
+      $scope.gridApi.grid.refresh();
+    });
+  }
+
+  function onRegisterApi(gridApi) {
+    $scope.gridApi = gridApi;
+    $scope.gridApi.grid.registerRowsProcessor($scope.singleFilter, 200);
+  }
+
+  function filter(renderableRows) {
+    var matcher = new RegExp($scope.filterValue);
+    renderableRows.forEach(function (row) {
+      if (!row.entity["name"].match(matcher)) {
+        row.visible = false;
       }
+    });
+    return renderableRows;
+  }
 
-      $scope.gridOptions.columnDefs = [
-        {name: 'name'},
-        {name: 'gender'},
-        {name: 'year'},
-        {name: 'details', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.showMovie()">View Me</button>'},
-        {name: 'edit', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.editMovie()">Edit Me</button>'},
-        {name: 'delete', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.deleteMovie(row)">Delete Me</button>'}
-      ];
+  function deleteMovie (row) {
+    console.log("delete Movie ",row);
+    // Movies.delete(row.entity._id);
+    row.entity.$delete();
+  }
 
-      $scope.gridOptions.data = [
-        {
-          name: "Pulp Fiction",
-          year: "1994",
-          gender: "action"
-        },
-        {
-          name: "The Godfather",
-          year: "1972",
-          gender: "action"
-        },
-        {
-          name: "Fight Club",
-          year: "1999",
-          gender: "action"
-        },
-        {
-          name: "Star Wars: Episode V - The Empire Strikes Back",
-          year: "1980",
-          gender: "Sci-Fi"
-        }
-      ];
+  function showMovie() {
+    alert('showMovie');
+  }
 
-      Movies.query(function(results){
-        console.log("Query",arguments);
-        results.forEach(function(result){
-          $scope.gridOptions.data.push(result);
-        })
-      });
+  function editMovie() {
+    alert('editMovie!');
+  }
 
-      $scope.$watch('filterValue', function () {
-        $scope.gridApi.grid.refresh();
-      });
-
-      $scope.singleFilter = function (renderableRows) {
-        var matcher = new RegExp($scope.filterValue);
-        renderableRows.forEach(function (row) {
-          if (!row.entity["name"].match(matcher)) {
-            row.visible = false;
-          }
-        });
-        return renderableRows;
-      };
-    }
-
-    function deleteMovie (row) {
-      console.log("delete Movie ",row);
-      // Movies.delete(row.entity._id);
-      row.entity.$delete();
-    }
-
-    function showMovie() {
-      alert('showMovie');
-    }
-
-    function editMovie() {
-      alert('editMovie!');
-    }
-
-  }]);
+}]);
 })();
