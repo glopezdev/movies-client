c4f523837384c5f7ccec1f1db8af97c43a5b1ebd
glopez <glopez@devspark.com>
2016-03-14 11:44:06 -0300

    imprive navegability

--- app/app.js
+++ app/app.js
@@ -1,24 +1,24 @@
 (function() {
     var app = angular.module('movieClientApp',
-        ['ui.router', 'movieClientAppList', 'movieClientAppEdit', 'movieClientAppDetails']);
-    app.config(["$stateProvider", "$urlRouterProvider",
+        ['ui.router','ngAnimate', 'movieClientAppList', 'movieClientAppEdit', 'movieClientAppDetails']);
+    app.config(['$stateProvider', '$urlRouterProvider',
         function($stateProvider, $urlRouterProvider) {
 
-            $urlRouterProvider.otherwise("/list");
+            $urlRouterProvider.otherwise('/list');
 
             $stateProvider
                 .state('list', {
-                    url: "/list",
-                    templateUrl: "list/list.html",
+                    url: '/list',
+                    templateUrl: 'list/list.html',
                     //    controller: "ListController" this loads the controller 2 times and gives an error
                 })
                 .state('edit', {
-                    url: "/edit/:id",
-                    templateUrl: "edit/edit.html"
+                    url: '/edit/:id',
+                    templateUrl: 'edit/edit.html'
                 })
                 .state('details', {
-                    url: "/details/:id",
-                    templateUrl: "details/details.html"
+                    url: '/details/:id',
+                    templateUrl: 'details/details.html'
                 })
         }
     ]);
